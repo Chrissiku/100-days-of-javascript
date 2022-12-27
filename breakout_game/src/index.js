21a557291c76a150d262b5ffb8775277f0fe0a72
Chris Siku <chrissiku5@gmail.com>
2022-12-27 10:54:19 +0200

    Create and draw the ball

--- breakout_game/src/index.js
+++ breakout_game/src/index.js
@@ -1,8 +1,23 @@
-import './styles/main.scss';
+import "./styles/main.scss";
 
-const showRules = document.getElementById('rules-btn');
-const closeRules = document.getElementById('close-btn');
-const rulesSideNav = document.getElementById('rules');
+const showRules = document.getElementById("rules-btn");
+const closeRules = document.getElementById("close-btn");
+const rulesSideNav = document.getElementById("rules");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-showRules.addEventListener('click', () => rulesSideNav.classList.add('show'));
-closeRules.addEventListener('click', () => rulesSideNav.classList.remove('show'));
+showRules.addEventListener("click", () => rulesSideNav.classList.add("show"));
+closeRules.addEventListener("click", () =>
+  rulesSideNav.classList.remove("show")
+);
+
+
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4,
+  visible: true,
+};
