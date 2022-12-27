8cb1a6bbaf822131164475dc14cbca3b72521df5
Chris Siku <chrissiku5@gmail.com>
2022-12-27 11:22:18 +0200

    Move the ball, increse the score, add wall boundaries and lose, reward and reset score

--- breakout_game/src/index.js
+++ breakout_game/src/index.js
@@ -6,6 +6,9 @@ const rulesSideNav = document.getElementById("rules");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 let score = 0;
+const brickRowCount = 9;
+const brickColumnCount = 5;
+const delay = 500;
 
 showRules.addEventListener("click", () => rulesSideNav.classList.add("show"));
 closeRules.addEventListener("click", () =>
@@ -26,7 +29,7 @@ const ball = {
 const drawBall = () => {
   ctx.beginPath();
   ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
-  ctx.fillStyle = ball.visible ? "#0095dd" : "transparent";
+  ctx.fillStyle = ball.visible ? "#06556d" : "transparent";
   ctx.fill();
   ctx.closePath();
 };
@@ -46,12 +49,12 @@ const paddle = {
 const drawPaddle = () => {
   ctx.beginPath();
   ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
-  ctx.fillStyle = paddle.visible ? "#0095dd" : "transparent";
+  ctx.fillStyle = paddle.visible ? "#06556d" : "transparent";
   ctx.fill();
   ctx.closePath();
 };
 
-// Create and drwa bricks
+// Create and draw bricks
 
 const brickInfo = {
   w: 70,
@@ -77,7 +80,7 @@ const drawBricks = () => {
     column.forEach((brick) => {
       ctx.beginPath();
       ctx.rect(brick.x, brick.y, brick.w, brick.h);
-      ctx.fillStyle = brick.visible ? "#0095dd" : "transparent";
+      ctx.fillStyle = brick.visible ? "#06556d" : "transparent";
       ctx.fill();
       ctx.closePath();
     });
@@ -105,8 +108,103 @@ const movePaddle = () => {
 };
 
 // Update canvas drawing and animation
+
+// Draw on the canvas
+const draw = () => {
+  // clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawBall();
+  drawPaddle();
+  drawScore();
+  drawBricks();
+};
+
+// Increase score
+const increaseScore = () => {
+  score++;
+
+  if (score % (brickRowCount * brickColumnCount) === 0) {
+    ball.visible = false;
+    paddle.visible = false;
+
+    //After 0.5 sec restart the game
+    setTimeout(function () {
+      showAllBricks();
+      score = 0;
+      paddle.x = canvas.width / 2 - 40;
+      paddle.y = canvas.height - 20;
+      ball.x = canvas.width / 2;
+      ball.y = canvas.height / 2;
+      ball.visible = true;
+      paddle.visible = true;
+    }, delay);
+  }
+};
+
+// Make all bricks appear
+const showAllBricks = () => {
+  bricks.forEach((column) => {
+    column.forEach((brick) => (brick.visible = true));
+  });
+};
+
+// Move ball on canvas
+const moveBall = () => {
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  // Wall collision (right/left)
+  if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+    ball.dx *= -1; // ball.dx = ball.dx * -1
+  }
+
+  // Wall collision (top/bottom)
+  if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+    ball.dy *= -1;
+  }
+
+  // console.log(ball.x, ball.y);
+
+  // Paddle collision
+  if (
+    ball.x - ball.size > paddle.x &&
+    ball.x + ball.size < paddle.x + paddle.w &&
+    ball.y + ball.size > paddle.y
+  ) {
+    ball.dy = -ball.speed;
+  }
+
+  // Brick collision
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      if (brick.visible) {
+        if (
+          ball.x - ball.size > brick.x && // left brick side check
+          ball.x + ball.size < brick.x + brick.w && // right brick side check
+          ball.y + ball.size > brick.y && // top brick side check
+          ball.y - ball.size < brick.y + brick.h // bottom brick side check
+        ) {
+          ball.dy *= -1;
+          brick.visible = false;
+
+          increaseScore();
+        }
+      }
+    });
+  });
+
+  // Hit bottom wall - Lose
+  if (ball.y + ball.size > canvas.height) {
+    showAllBricks();
+    score = 0;
+  }
+};
+
 const update = () => {
   movePaddle();
+  moveBall();
+  draw();
   requestAnimationFrame(update);
 };
 
