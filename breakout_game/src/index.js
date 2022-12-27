7eb0dc309a2e637c3a62ab702a6461f48cb2ab31
Chris Siku <chrissiku5@gmail.com>
2022-12-27 10:56:14 +0200

    Create bricks

--- breakout_game/src/index.js
+++ breakout_game/src/index.js
@@ -30,3 +30,22 @@ const paddle = {
   dx: 0,
   visible: true,
 };
+
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
