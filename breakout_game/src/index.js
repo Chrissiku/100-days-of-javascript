9a6b87f673179e860418d4992b1aa8487af33fa1
Chris Siku <chrissiku5@gmail.com>
2022-12-27 10:44:56 +0200

    Style the rules section

--- breakout_game/src/index.js
+++ breakout_game/src/index.js
@@ -1,1 +1,8 @@
 import './styles/main.scss';
+
+const showRules = document.getElementById('rules-btn');
+const closeRules = document.getElementById('close-btn');
+const rulesSideNav = document.getElementById('rules');
+
+showRules.addEventListener('click', () => rulesSideNav.classList.add('show'));
+closeRules.addEventListener('click', () => rulesSideNav.classList.remove('show'));
