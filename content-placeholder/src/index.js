35e7b40658a63717be79ad14ced10211ef00e8ee
Chris Siku <chrissiku5@gmail.com>
2022-12-28 10:01:15 +0200

    Add styling

--- content-placeholder/src/index.js
+++ content-placeholder/src/index.js
@@ -9,12 +9,12 @@ const date = document.getElementById("date");
 
 const getData = () => {
   header.innerHTML =
-    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />';
+    '<img src="https://media.istockphoto.com/id/1350595566/photo/cyber-security-firewall-interface-protection-concept-businesswoman-protecting-herself-from.jpg?b=1&s=170667a&w=0&k=20&c=wS9GoTWBNfsZVdO3FXZjogi9YrRRdBH_sqe6NjJrJ2U=" alt="post image" />';
   title.innerHTML = "Lorem ipsum dolor sit amet";
   excerpt.innerHTML =
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis";
   profile_img.innerHTML =
-    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />';
+    '<img src="https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584__340.png" alt="avatar" />';
   name.innerHTML = "John Doe";
   date.innerHTML = "Oct 08, 2020";
 };
