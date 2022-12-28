80e2050ef40501ad3e4dc4d61c8be4514df7e613
Chris Siku <chrissiku5@gmail.com>
2022-12-28 09:57:04 +0200

    Add data to by manupulation the DOM

--- content-placeholder/src/index.js
+++ content-placeholder/src/index.js
@@ -1,1 +1,22 @@
-import './styles/main.scss';
+import "./styles/main.scss";
+
+const header = document.querySelector("#header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_image = document.getElementById("profile_img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
+
+const getData = () => {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />';
+  title.innerHTML = "Lorem ipsum dolor sit amet";
+  excerpt.innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis";
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />';
+  name.innerHTML = "John Doe";
+  date.innerHTML = "Oct 08, 2020";
+};
+
+setTimeout(getData, 2500);
